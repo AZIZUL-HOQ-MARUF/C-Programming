fe8a9d854641fdc15a4ba5b9805845e4106ad9c6
AZIZUL HOQ <42655898+AZIZUL-HOQ-MARUF@users.noreply.github.com>
2019-09-12 01:51:29 +0600

    Developed Binery Search Tree Insertion, inOrder,PreOrder,postOrder lookUp and isPresent Function

---
+++ BinarySearchTree.cpp
@@ -0,0 +1,98 @@
+#include<iostream>
+using namespace std;
+class Node{
+
+public:
+    Node* left;
+    int data;
+    Node* right;
+
+    Node(int d){
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+class BST{
+public:
+    Node* root;
+    bool flag = false;
+    BST(){
+        root = NULL;
+    }
+    void insertNode(int d){
+        Node* newNode = new Node(d);
+
+        if(root == NULL){
+            root = newNode;
+        }else{
+            Node* node = root;
+            while(1){
+                if(d<node->data){
+                    if(node->left == NULL){
+                        node->left = newNode;
+                        return;
+                    }
+                    node = node->left;
+                }else{
+                    if(node->right == NULL){
+                        node->right = newNode;
+                        return;
+                    }
+                    node = node->right;
+                }
+            }
+        }
+    }
+    void inOrderLookUp(Node* x){
+        if(x != NULL){
+            inOrderLookUp(x->left);
+            cout << x->data << "\n";
+            inOrderLookUp(x->right);
+        }
+    }
+    void preOrderLookUp(Node* x){
+        if(x != NULL){
+            cout << x->data << "\n";
+            inOrderLookUp(x->left);
+            inOrderLookUp(x->right);
+        }
+    }
+    void postOrderLookUp(Node* x){
+        if(x != NULL){
+            inOrderLookUp(x->left);
+            inOrderLookUp(x->right);
+            cout << x->data << "\n";
+        }
+    }
+    bool isPresent(Node* x,int d){
+        if(x != NULL){
+            isPresent(x->left,d);
+            if(x->data == d){
+                flag = true;
+                return flag;
+            }
+            isPresent(x->right,d);
+        }
+        return flag;
+    }
+
+};
+int main(){
+    BST bst;
+    bst.insertNode(10);
+    bst.insertNode(5);
+    bst.insertNode(12);
+    bst.insertNode(11);
+    bst.insertNode(4);
+    bst.insertNode(32);
+    if(bst.isPresent(bst.root,4)){
+        cout <<"present\n";
+    }
+    else{
+        cout <<"not present\n";
+    }
+
+    bst.inOrderLookUp(bst.root);
+
+}
